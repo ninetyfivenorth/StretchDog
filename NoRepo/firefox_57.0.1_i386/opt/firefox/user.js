ea3f04b172fc9ff46e1adb18fd0b8a7a8f9ed689
fredx181 <f.blom18@chello.nl>
2017-11-30 01:10:15 +0000

    Updated packages repo

---
+++ NoRepo/firefox_57.0.1_i386/opt/firefox/user.js
@@ -0,0 +1,4 @@
+// for using apulse with latest firefox
+user_pref("security.sandbox.content.write_path_whitelist", "/dev/snd/");
+user_pref("security.sandbox.content.syscall_whitelist", "/dev/snd/");
+user_pref("security.sandbox.content.read_path_whitelist", "/dev/snd/");
